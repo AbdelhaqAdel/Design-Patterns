50180d82b3fdf719df00bff96344daaac3bda879
abdelhaq <147981005+AbdelhaqAdel@users.noreply.github.com>
2024-08-21 18:01:58 +0300

    add new creational pattern (factory pattern)

---
+++ Design patterns/crerational patterns/Factory pattern/factory_main.dart
@@ -0,0 +1,15 @@
+//factory pattern :
+//make the user can use all type of banks with one object
+
+import 'bank.dart';
+import 'factory.dart';
+void main(){
+BankFactory bankFactory=new BankFactory();
+IBank bank1=bankFactory.GetBank('12345');
+IBank bank2=bankFactory.GetBank('11111');
+
+//bank.GetBank('12345888');
+//bank.GetBank('111112535');
+bank1.whithDrow();
+bank2.whithDrow();
+}
